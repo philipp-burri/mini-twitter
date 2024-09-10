747659718f02a4e96ebeddc9936bd5c54207f327
philipp-burri <burri.philipp13@gmail.com>
2024-09-10 15:13:24 +0200

    first commit

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 
 Route::get('/users-info', [UserController::class, 'index']);
-
+Route::get('/posts', [PostController::class, 'index']);
